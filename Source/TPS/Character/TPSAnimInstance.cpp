9a075ffb0e8ad9908bff2764fadd90e3b702ce52
ivxxvii <ryan14075@gmail.com>
2025-07-26 21:36:51 +0800

    UE5 Online TPS Game Leaning And Strafing Animation Setup

--- Source/TPS/Character/TPSAnimInstance.cpp
+++ Source/TPS/Character/TPSAnimInstance.cpp
@@ -4,6 +4,7 @@
 #include "TPSAnimInstance.h"
 #include "TPSCharacter.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
 void UTPSAnimInstance::NativeInitializeAnimation()
 {
@@ -12,9 +13,9 @@ void UTPSAnimInstance::NativeInitializeAnimation()
 	TPSCharacter = Cast<ATPSCharacter>(TryGetPawnOwner());
 }
 
-void UTPSAnimInstance::NativeUpdateAnimation(float Deltatime)
+void UTPSAnimInstance::NativeUpdateAnimation(float DeltaTime)
 {
-	Super::NativeUpdateAnimation(Deltatime);
+	Super::NativeUpdateAnimation(DeltaTime);
 
 	if (TPSCharacter == nullptr)
 	{
@@ -32,4 +33,19 @@ void UTPSAnimInstance::NativeUpdateAnimation(float Deltatime)
 	bWeaponEquipped = TPSCharacter->IsWeaponEquipped();
 	bIsCrouched = TPSCharacter->bIsCrouched;
 	bAiming = TPSCharacter->IsAiming();
+
+	//Offset yaw For Strafing
+	FRotator AimRotation = TPSCharacter->GetBaseAimRotation();
+	FRotator MovementRotation = UKismetMathLibrary::MakeRotFromX(TPSCharacter->GetVelocity());
+	FRotator DeltaRot = UKismetMathLibrary::NormalizedDeltaRotator(MovementRotation, AimRotation);
+	DeltaRotation = FMath::RInterpTo(DeltaRotation, DeltaRot, DeltaTime, 6.f);
+	YawOffset = DeltaRotation.Yaw;
+
+	CharacterRotationLastFrame = CharacterRotation;
+	CharacterRotation = TPSCharacter->GetActorRotation();
+	const FRotator Delta = UKismetMathLibrary::NormalizedDeltaRotator(CharacterRotation, CharacterRotationLastFrame);
+	const float Target = Delta.Yaw / DeltaTime;
+	const float interp = FMath::FInterpTo(Lean, Target, DeltaTime, 6.f);
+	Lean = FMath::Clamp(interp, -90.f, 90.f);
+
 }
--- Source/TPS/Character/TPSAnimInstance.h
+++ Source/TPS/Character/TPSAnimInstance.h
@@ -16,7 +16,7 @@ class TPS_API UTPSAnimInstance : public UAnimInstance
 	
 public:
 	virtual void NativeInitializeAnimation() override;
-	virtual void NativeUpdateAnimation(float Deltatime) override;
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
 
 private:
 	UPROPERTY(BlueprintReadOnly, Category = Character, meta = (AllowPrivateAccess = "true"))
@@ -40,4 +40,13 @@ private:
 	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	bool bAiming;
 
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float YawOffset;
+
+	UPROPERTY(BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	float Lean;
+
+	FRotator CharacterRotationLastFrame;
+	FRotator CharacterRotation;
+	FRotator DeltaRotation;
 };
